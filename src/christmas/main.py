3061657ec6f0299258393133c421b5207a63b09d
akran <nhlk123@naver.com>
2025-03-02 01:27:08 +0900

    feat: 가격 확인 기능 추가

--- src/christmas/main.py
+++ src/christmas/main.py
@@ -1,16 +1,18 @@
 from input_view import InputView
 
 
+
+def int_verification(x) :
+    if not x.isdigit() :
+        raise ValueError("[ERROR] 숫자를 입력하세요")
+    
 def date_verification(date) :
-    if not date.isdigit() :
-            raise ValueError("[ERROR] 숫자를 입력하세요")
-        
+    int_verification(date)
     date=int(date)
     if date<1 or date>31 :
         raise ValueError("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해주세요")
 
-
-def menu_verification(menu) :
+def menu_list_verification(menu) :
     menu_list = [
     "양송이수프",
     "타파스",
@@ -25,25 +27,51 @@ def menu_verification(menu) :
     "레드와인",
     "샴페인"
     ]
-    for i in range(len(menu)) :
-        a = menu[i]
-        a= a.split('-')
-        menu[i]=a
-        
-        if menu[i][0] not in menu_list :
-            raise ValueError("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
-        if not menu[i][1].isdigit() :
-            raise ValueError("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
 
+    if menu not in menu_list :
+        raise ValueError("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+
+    
+def menucount(count) :
+    if count>20 :
+        raise ValueError("[ERROR] 20개 이상 주문할 수 없습니다")
+
+        
+def menu_verification(menu) :
+    menu_set = set()
+    count=0
+    menu = [item.split('-') for item in menu]
+    print(menu)
+    for i in range(len(menu)) :
+        menu_list_verification(menu[i][0])
+        menu_set.add(menu[i][0])
+        count+=int(menu[i][1])
+        int_verification(menu[i][1])
+        if int(menu[i][1])<1 :
+            raise ValueError("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+    if len(menu_set)!=len(menu) :
+            raise ValueError("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+    menucount(count)
     return menu
 
+def price_count(menu) :
+    menu_dict = {
+        "양송이수프": 6000, "타파스": 5500, "시저샐러드": 8000,
+        "티본스테이크": 55000, "바비큐립": 54000, "해산물파스타": 35000,
+        "크리스마스파스타": 25000, "초코케이크": 15000, "아이스크림": 5000,
+        "제로콜라": 3000, "레드와인": 60000, "샴페인": 25000
+    }
+    price = 0
+    for i in range(len(menu)):
+        price+=int(menu[i][1])*menu_dict[menu[i][0]]
 
+    return price
+    
 
 def main():
     menu = InputView.menu_ask()
-    menu_verification(menu)
-    print(menu)
-    pass
-
+    a=menu_verification(menu)
+    price =price_count(a)
+    print(price)
 if __name__ == "__main__":
     main()
